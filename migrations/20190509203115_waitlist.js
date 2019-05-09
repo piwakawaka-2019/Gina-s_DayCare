dc462da30eca556b971c1fdd6812caaa3acf4b81
zozo-url <zozo-url@protonmail.ch>
2019-05-09 21:27:45 +1200

    create waitlist and ecc table

--- migrations/20190509203115_waitlist.js
+++ migrations/20190509203115_waitlist.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('waitlist', table => {
+      table.increments('child_id')
+      table.increments('ecc_id')
+      table.increments('rank_ecc')
+      table.increments('rank_parent')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('waitlist')
 };
