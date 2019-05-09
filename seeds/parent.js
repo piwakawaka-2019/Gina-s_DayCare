e952b378177fee524edec1645e2f9cfd40a9f9b5
Devon Lumsden <devonlumsden@mail.com>
2019-05-09 23:54:26 +1200

    add migrations/seeds for parent & child tables

--- seeds/parent.js
+++ seeds/parent.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('parent').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('parent').insert([
+        {id: 1, first_name: 'Devon', last_name: 'Longbottom', username: 'BigDog007', hash_password: 'bigdog', email: 'bigdogggottaeat@example.com'},
+        {id: 2, first_name: 'Lucy', last_name: 'Mcgoose', username: 'isLoose353', hash_password: 'asagoose', email: 'lucymcloose@example.com'},
+        {id: 3, first_name: 'Graeham', last_name: 'Grey', username: 'theGraeham', hash_password: 'mynameisbland', email: 'mynameisbland@example.com'}
       ]);
     });
 };
